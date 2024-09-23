5bbafe16128c3665bc2e2d135dcd6f0b1a54a2c7
Joelma Skalecki dos Santos <133382445+skaleckijoh17@users.noreply.github.com>
2024-09-23 10:40:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Joelma Nutrição";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciemte.querySelector(".info-peso");
+var peso = tdPeso.textContent = "80";
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent = ;
